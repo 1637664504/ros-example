1628a0bc4109ef4e317d01787930e1c9c0bc100c
liuJiang <lj1637664504@outlook.com>
2023-06-01 18:08:26 +0800

    添加tf转换

--- ros1/src/1_ros_基础/ch4_launch/setup.py
+++ ros1/src/1_ros_基础/ch4_launch/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['4_launch'],
+    packages=['ch4_launch'],
     package_dir={'': 'src'}
 )
 
