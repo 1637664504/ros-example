f8c8bbc16c1194d2849ff17e492c1b84532d855c
liuJiang <lj1637664504@outlook.com>
2024-01-12 19:24:20 +0800

    [service]请求超时处理

--- ros1/src/1_ros_基础/ch8_srv_service/src/6_client_string_waitFor请求超时.cc
+++ ros1/src/1_ros_基础/ch8_srv_service/src/6_client_string_waitFor请求超时.cc
@@ -7,6 +7,21 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "test_client_string");
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<common::diagInfo>("/test_string");
+
+    // 一直超时
+    // client.waitForExistence();
+
+    // 超时5S
+    // if (!client.waitForExistence(ros::Duration(5.0))) {
+    //     ROS_ERROR("Service did not exist");
+    //     return -1;
+    // }
+
+    // 超时5s
+    ros::service::waitForService("/test_string", ros::Duration(5.0));
+
+    ROS_INFO("Waiting for service up");
+
     common::diagInfo srv;
     srv.request.cmd = "get";
     if (client.call(srv))
